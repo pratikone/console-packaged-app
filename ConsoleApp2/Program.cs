655dc60605fff054804935434ae9f579e498c5ce
Pratik Anand <praanan@microsoft.com>
2025-05-14 11:14:29 -0700

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Windows.AI;
+using Microsoft.Windows.AI.Generative;
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+// Initialize the AI model
+if (LanguageModel.GetReadyState() == AIFeatureReadyState.EnsureNeeded)
+{
+   await LanguageModel.EnsureReadyAsync();
+}
+LanguageModel model = await LanguageModel.CreateAsync();
+
+LanguageModelResponseResult response = await model.GenerateResponseAsync("What is the capital of France?");
+Console.WriteLine(response.Text);
+
+
